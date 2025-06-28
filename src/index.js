3adfc938a0a5b05234432a133314641f09ac48ac
Luis Filipe M. dos Santos <163148824+luisfilipems28@users.noreply.github.com>
2025-06-28 17:00:40 -0300

    Prompt de usuario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// Caso não tenha, baixar o pacote "npm i readline-sync"
+
 const prompt = require("readline-sync");
 
-const usuario = String(prompt.question("Qual é o seu nome?"));
+const nomeDoUsuario = prompt.question("Qual é o seu nome? ");
+
+if (nomeDoUsuario) {
+  console.log(`Olá ${nomeDoUsuario}, seja bem vindo`);
+} else {
+  console.log(
+    "Parece que você digitou o nome errado. Por favor, tente novamente"
+  );
+}
