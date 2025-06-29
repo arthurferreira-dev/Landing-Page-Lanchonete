716651531491e95a0f20fe8f6880b3f0ccf5326d
Arthur Ferreira <tutudacosta996@gmail.com>
2025-06-28 22:13:46 -0300

    Organizando o projeto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-let nome = prompt("Qual é o seu nome?");
+function Name() {
+  let welcomeTXT = document.getElementById('bemVindo');
+  let nome = prompt("Qual é o seu nome?");
+  if (nome == 'Arthur Ferreira' | nome == 'Arthur' | nome == 'Ferreira' | nome == 'arthur ferreira' | nome == 'arthur' | nome == 'ferreira') {
+    welcomeTXT.innerHTML = '[EasterEGG] Olá Arthur Ferreira Dev, você mesmo programou isso! [EasterEGG]'
+  } else if (nome == 'Jack Bila' | nome == 'Jack' | nome == 'Bila' | nome == 'jack bila' | nome == 'jack' | nome == 'bila') {
+    welcomeTXT.innerHTML = '[EasterEGG] Eae JackBila, bom trabalho com o SASS! [EasterEGG]'
+  } else if (nome == 'Luis Felipe' | nome == 'Luis' | nome == 'Felipe' | nome == 'luis felipe' | nome == 'Luis' | nome == 'felipe') {
+    welcomeTXT.innerHTML = '[EasterEGG] Oi Luis Felipe, belo "index.js"! [EasterEGG]'
+  } else if (nome == 'Theo Teixeira Kobel' | nome == 'Theo Teixeira' | nome =='Theo' | nome == 'Kobel' | nome == 'theo teixeira kobel' | nome == 'theo teixeira' | nome =='theo' | nome == 'kobel') {
+    welcomeTXT.innerHTML = '[EasterEGG] Eae <strong>TTK</strong>, de boa? [EasterEGG]'
+  } else {
+    welcomeTXT.innerHTML = `Olá ${nome}, seja bem vindo!`
+  }
+}
+
 
 function carregar() {
-  let msg = document.getElementById("timer");
+  let msg = document.getElementById('timer');
   timer.innerHTML = `Agora são ${hora} horas e ${min} minutos !`;
   let h = new Date();
   let m = new Date();
