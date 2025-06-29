2c51c9fdb24b859048e695f18ef535843c6e18fa
Arthur Ferreira <tutudacosta996@gmail.com>
2025-06-29 18:18:56 -0300

    melhorando o index.html

--- src/index.js
+++ src/index.js
@@ -8,55 +8,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
   welcomeMessageElement.textContent = `Olá ${nome}, seja bem vindo!`;
 });
-function Name() {
-  let welcomeTXT = document.getElementById("bemVindo");
-  let nome = prompt("Qual é o seu nome?");
-  if (
-    (nome == "Arthur Ferreira") |
-    (nome == "Arthur") |
-    (nome == "Ferreira") |
-    (nome == "arthur ferreira") |
-    (nome == "arthur") |
-    (nome == "ferreira")
-  ) {
-    welcomeTXT.innerHTML =
-      "[EasterEGG] Olá Arthur Ferreira Dev, você mesmo programou isso! [EasterEGG]";
-  } else if (
-    (nome == "Jack Bila") |
-    (nome == "Jack") |
-    (nome == "Bila") |
-    (nome == "jack bila") |
-    (nome == "jack") |
-    (nome == "bila")
-  ) {
-    welcomeTXT.innerHTML =
-      "[EasterEGG] Eae JackBila, bom trabalho com o SASS! [EasterEGG]";
-  } else if (
-    (nome == "Luis Felipe") |
-    (nome == "Luis") |
-    (nome == "Felipe") |
-    (nome == "luis felipe") |
-    (nome == "Luis") |
-    (nome == "felipe")
-  ) {
-    welcomeTXT.innerHTML =
-      '[EasterEGG] Oi Luis Felipe, belo "index.js"! [EasterEGG]';
-  } else if (
-    (nome == "Theo Teixeira Kobel") |
-    (nome == "Theo Teixeira") |
-    (nome == "Theo") |
-    (nome == "Kobel") |
-    (nome == "theo teixeira kobel") |
-    (nome == "theo teixeira") |
-    (nome == "theo") |
-    (nome == "kobel")
-  ) {
-    welcomeTXT.innerHTML =
-      "[EasterEGG] Eae <strong>TTK</strong>, de boa? [EasterEGG]";
-  } else {
-    welcomeTXT.innerHTML = `Olá ${nome}, seja bem vindo!`;
+
+  function Name() {
+    let welcomeTXT = document.getElementById("bemVindo");
+    let nome = prompt("Qual é o seu nome?");
+    if (
+      (nome == "Arthur Ferreira") |
+      (nome == "Arthur") |
+      (nome == "Ferreira") |
+      (nome == "arthur ferreira") |
+      (nome == "arthur") |
+      (nome == "ferreira")
+    ) {
+      welcomeTXT.innerHTML =
+        "[EasterEGG] Olá Arthur Ferreira Dev, você mesmo programou isso! [EasterEGG]";
+    } else if (
+      (nome == "Jack Bila") |
+      (nome == "Jack") |
+      (nome == "Bila") |
+      (nome == "jack bila") |
+      (nome == "jack") |
+      (nome == "bila")
+    ) {
+      welcomeTXT.innerHTML =
+        "[EasterEGG] Eae JackBila, bom trabalho com o SASS! [EasterEGG]";
+    } else if (
+      (nome == "Luis Felipe") |
+      (nome == "Luis") |
+      (nome == "Felipe") |
+      (nome == "luis felipe") |
+      (nome == "Luis") |
+      (nome == "felipe")
+    ) {
+      welcomeTXT.innerHTML =
+        '[EasterEGG] Oi Luis Felipe, belo "index.js"! [EasterEGG]';
+    } else if (
+      (nome == "Theo Teixeira Kobel") |
+      (nome == "Theo Teixeira") |
+      (nome == "Theo") |
+      (nome == "Kobel") |
+      (nome == "theo teixeira kobel") |
+      (nome == "theo teixeira") |
+      (nome == "theo") |
+      (nome == "kobel")
+    ) {
+      welcomeTXT.innerHTML =
+        "[EasterEGG] Eae <strong>TTK</strong>, de boa? [EasterEGG]";
+    } else {
+      welcomeTXT.innerHTML = `Olá ${nome}, seja bem vindo!`;
+    }
   }
-}
 
 function carregar() {
   let timerElement = document.getElementById("timer"); // Renomeei a variável para evitar conflito
